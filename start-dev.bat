6059a1ef39458d53821370f1e76cf0f8ecdb6f4c
Thomas Akiou <thomasakiou@gmail.com>
2025-12-18 09:05:35 +0100

    push

--- start-dev.bat
+++ start-dev.bat
@@ -2,7 +2,7 @@
 REM Auto-start NECO Payment Manager Frontend
 REM This script starts the development server automatically
 
-cd /d "G:\Projects\neco-payment-manager-FE"
+cd /d "C:\apps\neco-payment-manager\neco-payment-manager-FE"
 
 REM Start the development server
 start "NECO Payment Manager - Frontend" cmd /k npm run dev
